af7e88c3aaa41364948094ce668e0f94a83b3bdb
Chris <comosjay@gmail.com>
2022-12-03 19:20:00 -0800

    update

--- JetProjectPractice/src/myFrame.java
+++ JetProjectPractice/src/myFrame.java
@@ -1,23 +1,33 @@
+// import java.awt.BorderLayout;
+// import java.awt.GridLayout;
+// import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-
-
-
-
+    
+                        
+            
+                                         
+      
 public class myFrame extends JFrame{
     seconds panel;
-    
-    myFrame()
-    {
+    int a = 1;     
+    myFrame()                             
+    { 
         panel = new seconds();
-        this.add(panel);
+        // panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        // panel.setLayout(new GridLayout(0,1));
+        // label.setText("deg: " + panel.getDeg());
+        
+        // panel.add(label, BorderLayout.CENTER); 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(panel);      
         this.pack();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-        
-       
+         
+             
+                    
 
-        
+         
     }
     
 }
--- JetProjectPractice/src/seconds.java
+++ JetProjectPractice/src/seconds.java
@@ -1,49 +1,127 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
-
-public class seconds extends JPanel implements ActionListener{
-    int x = 0;
-    int y = 0;
-    int velX = 2;
-    int velY = 2;
-    Image image;
+import java.awt.event.KeyListener; 
+//player
+public class seconds extends JPanel implements ActionListener, KeyListener{
+    double x1 = 0;
+    double y1 = 0;
+    int a = 0;
+    double velX = 5;
+    double velY = 5;
+    double Deg1 = 0;
+    double DegVel = 3;
+    Image image1, bullet;
     Timer timer;
-    JLabel label;
+    JLabel label; 
     seconds()
     {
-        this.setPreferredSize(new Dimension(500,500));
-        this.setBackground(Color.BLACK);
-        image = new ImageIcon("plane.jpg").getImage();
+        this.setPreferredSize(new Dimension(600,600));
+        this.setBackground(Color.BLACK);  
+        image1 = new ImageIcon("plane.jpg").getImage();
+        // bullet = new ImageIcon("plane2.jpg").getImage();
         timer = new Timer(1, this);
         timer.start();
+        addKeyListener(this);
+        setFocusable(true);
+        // setLayout(new GridLayout(0,1));
 
     }
     
+  
 
-
-    public void paint(Graphics g)
+    public void paint(Graphics g) 
     {
         super.paint(g);
         Graphics2D g2D = (Graphics2D) g;
-        g2D.drawImage(image,x,y,null);
+        goingTheSameWay();
+        g2D.rotate(Math.toRadians(Deg1),(image1.getWidth(label) / 2) + x1 ,(image1.getHeight(label) / 2) +y1);
+        g2D.drawImage(image1,(int)x1,(int)y1,null); 
+
 
     }
 
 
 
     @Override
-    public void actionPerformed(ActionEvent e) 
-    {
-        if(x >= 500)
-        {
-            velX = velX * -1;
-        }
-        x += velX;
+    public void actionPerformed(ActionEvent e)  
+    {   
+        
         
         repaint();
+       
+
+        
     }
+
+    public void keyPressed(KeyEvent e)
+    {
+        int key = e.getKeyCode();
+        
+
+        if(key == KeyEvent.VK_RIGHT)
+        {
+            Deg1 = Deg1 + DegVel;
+           
+
+        }
+        if(key == KeyEvent.VK_LEFT)
+        {
+            Deg1 = Deg1 - DegVel;
+        }
+        
+
+
+   
+        
+
+        
+    }
+
+    public Boolean getImage()
+    {
+        if(image1 == null)
+        {
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
+    public void goingTheSameWay()
+    {
+        x1 += velX * Math.cos(Math.toRadians(Deg1));
+        y1 += velY * Math.sin(Math.toRadians(Deg1));
+
+        
+    }
+
+    public void keyReleased(KeyEvent e)
+    {
+        
+
+    }
+
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    // public double GetX()
+    // {
+    //     return x1;
+    // }
+    // public double GetY()
+    // {
+    //     return y1;
+    // }
+    // public double getDeg()
+    // {
+    //     return Deg1;
+    // } 
 
 
 
